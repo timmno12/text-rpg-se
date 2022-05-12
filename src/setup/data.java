9ab007c6b55ac3bdb2d643e891f26c129044ac82
Maibo1608 <Maik.Borowski@haw-hamburg.de>
2022-05-12 12:04:35 +0200

    Item Description erstes Beispiel

--- src/setup/data.java
+++ src/setup/data.java
@@ -5,8 +5,13 @@ import java.util.Arrays;
 import java.util.List;
 
 public class data {
+    public static Item[] items = {
+            new Item("piece of wire", "Whoever kidnapped you must’ve underestimated the power of a piece of wire because this piece of wire is your trusty lock-pick.", 0, 0),
+            new Item("item2", "its an item 2 yo", 0, 0)
+
+    };
     public static Target[] DATA={
-            new Target("Cage","piece of wire", "look", "Whoever kidnapped you must’ve underestimated the power of a piece of wire because this piece of wire is your trusty lock-pick.", 10,false),
+            new Target("Cage","piece of wire", "look", items[0].getItemDescription(), 10,false),
             new Target("Cage","piece of wire", "use", "After fiddling around for a little you manage to unlock your cage.", 10,true),
             new Target("Cage","", "look", "It’s dark here, the only light coming from the cracks in the door. You look outside your cage and down the shelf you're on, the distance is not too bad. On your right is your sister's cage, she seems nervous and is fiddling around with the metal bars. There’s a big jar to your left which is filled with suspiciously glowing water. ", 10,false),
             new Target("Cage","sister", "talk", "I’m so scared, I want to go home.", 0,false),
@@ -43,11 +48,7 @@ public class data {
 
 
     };
-    public static Item[] items = {
-            new Item("item", "its an item yo", 0, 0),
-            new Item("item2", "its an item 2 yo", 0, 0)
 
-    };
 
 
 }

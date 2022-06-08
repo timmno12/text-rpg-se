725c369460af6d997416e4cd2fad5161ab86ef33
Ithesa <rebekkan2@web.de>
2022-06-08 15:12:11 +0200

    Data - added Player voicelines in conversation Chapter 2 (lion) StoryTracker - added targets_3 to targets_7

--- src/setup/StoryTracker.java
+++ src/setup/StoryTracker.java
@@ -7,12 +7,12 @@ public class StoryTracker {
 
     private String[] targets_0= {"sister","piece of wire"};
     private String[] targets_1= {"sister","piece of wire","door"};
-    private String[] targets_2= {"sister","piece of wire", "arched entryway", "cat"};
-    private String[] targets_3= {"sister","piece of wire", "arched entryway", "cat", "door ahead", "right door", "left door", "door behind", "raven", "button"};
-    private String[] targets_4= {"sister","piece of wire", "arched entryway", "cat", "door ahead", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble"};
-    private String[] targets_5= {"sister","piece of wire", "arched entryway", "cat", "door ahead", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "chapel", "cloth", "lion"};
-    private String[] targets_6= {"sister","piece of wire", "arched entryway", "cat", "door ahead", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "chapel", "cloth", "lion"};
-    private String[] targets_7= {"sister","piece of wire", "arched entryway", "cat", "door ahead", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "chapel", "cloth", "lion"};
+    private String[] targets_2= {"sister","piece of wire", "door ahead", "cat"};
+    private String[] targets_3= {"sister","piece of wire", "door ahead", "cat", "right door", "left door", "door behind", "raven", "button"};
+    private String[] targets_4= {"sister","piece of wire", "door ahead", "cat", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble"};
+    private String[] targets_5= {"sister","piece of wire", "door ahead", "cat", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "chapel", "cloth", "lion"};
+    private String[] targets_6= {"sister","piece of wire", "door ahead", "cat", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "chapel", "cloth", "lion"};
+    private String[] targets_7= {"sister","piece of wire", "door ahead", "cat", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "chapel", "cloth", "lion"};
     private String[][] targets = {targets_0, targets_1, targets_2, targets_3, targets_4, targets_5, targets_6, targets_7};
 
     public void progress(){

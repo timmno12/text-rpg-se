920a125a4f44e3eea58d8bdb1fcabe64e8a3a573
Ithesa <rebekkan2@web.de>
2022-06-08 15:07:17 +0200

    Data - added Player voicelines in conversation Chapter 2 (lion) StoryTracker - added targets_3 to targets_7

--- src/setup/StoryTracker.java
+++ src/setup/StoryTracker.java
@@ -3,14 +3,17 @@ package setup;
 public class StoryTracker {
     private int tracker = 0;
 
-    private String[] locations = {"Cage","Storage Room", "Hallway (Basement)"};
+    private String[] locations = {"Cage","Storage Room", "Hallway (Basement)", "Hallway (Basement)", "Eerie Crypt (left room)", "Eerie Crypt (left room)", "Eerie Crypt (left room)"};
 
     private String[] targets_0= {"sister","piece of wire"};
     private String[] targets_1= {"sister","piece of wire","door"};
     private String[] targets_2= {"sister","piece of wire", "arched entryway", "cat"};
-    private String[] targets_3= {"sister","piece of wire", "arched entryway", "cat"};
-
-    private String[][] targets = {targets_0, targets_1, targets_2,targets_3};
+    private String[] targets_3= {"sister","piece of wire", "arched entryway", "cat", "door ahead", "right door", "left door", "door behind", "raven", "button"};
+    private String[] targets_4= {"sister","piece of wire", "arched entryway", "cat", "door ahead", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble"};
+    private String[] targets_5= {"sister","piece of wire", "arched entryway", "cat", "door ahead", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "chapel", "cloth", "lion"};
+    private String[] targets_6= {"sister","piece of wire", "arched entryway", "cat", "door ahead", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "chapel", "cloth", "lion"};
+    private String[] targets_7= {"sister","piece of wire", "arched entryway", "cat", "door ahead", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "chapel", "cloth", "lion"};
+    private String[][] targets = {targets_0, targets_1, targets_2, targets_3, targets_4, targets_5, targets_6, targets_7};
 
     public void progress(){
        tracker +=1;

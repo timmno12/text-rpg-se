bbd4151504ff0af2192fa959475ceecd514b79da
Ithesa <rebekkan2@web.de>
2022-06-13 23:29:59 +0200

    Data & StoryTracker - removed "chapel" as object, (Not needed anymore since it became a location)

--- src/setup/StoryTracker.java
+++ src/setup/StoryTracker.java
@@ -22,31 +22,31 @@ public class StoryTracker {
     private String[] targets_4= {"sister","piece of wire", "door ahead", "cat", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble"};
 
     //Ruins of a crumbled church
-    private String[] targets_5= {"sister","piece of wire", "door ahead", "cat", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "chapel", "cloth", "lion"};
+    private String[] targets_5= {"sister","piece of wire", "door ahead", "cat", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "cloth", "lion"};
 
     //Ruins of a crumbled church <- Mit Leerzeichen hinten dran
-    private String[] targets_6= {"sister","piece of wire", "door ahead", "cat", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "chapel", "cloth", "lion"};
+    private String[] targets_6= {"sister","piece of wire", "door ahead", "cat", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "cloth", "lion"};
 
     //Hallway (Basement) III
-    private String[] targets_7= {"sister","piece of wire", "door ahead", "cat", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "chapel", "cloth", "lion", "dandelion"};
+    private String[] targets_7= {"sister","piece of wire", "door ahead", "cat", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "cloth", "lion", "dandelion"};
 
     //Hallway (Basement) IV
-    private String[] targets_8= {"sister","piece of wire", "door ahead", "cat", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "chapel", "cloth", "lion", "dandelion"};
+    private String[] targets_8= {"sister","piece of wire", "door ahead", "cat", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "cloth", "lion", "dandelion"};
 
     //Foggy void
-    private String[] targets_9= {"sister","piece of wire", "door ahead", "cat", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "chapel", "cloth", "lion", "dandelion", "ahead", "right", "left"};
+    private String[] targets_9= {"sister","piece of wire", "door ahead", "cat", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "cloth", "lion", "dandelion", "ahead", "right", "left"};
 
     //Foggy void II
-    private String[] targets_10= {"sister","piece of wire", "door ahead", "cat", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "chapel", "cloth", "lion", "dandelion", "ahead", "right", "left"};
+    private String[] targets_10= {"sister","piece of wire", "door ahead", "cat", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "cloth", "lion", "dandelion", "ahead", "right", "left"};
 
     //Foggy void III
-    private String[] targets_11= {"sister","piece of wire", "door ahead", "cat", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "chapel", "cloth", "lion", "dandelion", "ahead", "right", "left"};
+    private String[] targets_11= {"sister","piece of wire", "door ahead", "cat", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "cloth", "lion", "dandelion", "ahead", "right", "left"};
 
     //Light flooded cave
-    private String[] targets_12= {"sister","piece of wire", "door ahead", "cat", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "chapel", "cloth", "lion", "dandelion", "ahead", "right", "left", "cat"};
+    private String[] targets_12= {"sister","piece of wire", "door ahead", "cat", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "cloth", "lion", "dandelion", "ahead", "right", "left", "cat"};
 
     //Hallway (Basement) V
-    private String[] targets_13= {"sister","piece of wire", "door ahead", "cat", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "chapel", "cloth", "lion", "dandelion", "ahead", "right", "left", "cat"};
+    private String[] targets_13= {"sister","piece of wire", "door ahead", "cat", "right door", "left door", "door behind", "raven", "button", "pedestal", "marble", "cloth", "lion", "dandelion", "ahead", "right", "left", "cat"};
 
     private String[][] targets = {targets_0, targets_1, targets_2, targets_3, targets_4, targets_5, targets_6, targets_7, targets_8, targets_9, targets_10, targets_11, targets_12, targets_13};
 

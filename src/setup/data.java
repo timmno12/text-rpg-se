b5f8ec034d5dc3254ce855fe5c64ab52cfe35ec3
Ithesa <rebekkan2@web.de>
2022-06-15 17:12:52 +0200

    Data & StoryTracker - added button and new location + Target Data - added Discription "Chapter 2" to dialogue attack cat and end conversation with cat

--- src/setup/data.java
+++ src/setup/data.java
@@ -39,8 +39,8 @@ public class data {
 
             new Target("Storage Room","sister","", "talk", "Let's get out of here!", 10,false),
             new Target("Storage Room","piece of wire", "cage","combine", "You successfully unlock your sister's cage as well.", 10,true),
-            new Target("Storage Room","", "look", "","This is definitely a storage room. Lots of shelves filled with gruesome things you don’t really want to look at closely. The door seems like the only way out.", 10,false),
-            new Target("Storage Room","door", "interact","", "Your sister yells out in protest. Were you gonna leave without her?", 10,false),
+            new Target("Storage Room","", "", "look","This is definitely a storage room. Lots of shelves filled with gruesome things you don’t really want to look at closely. The door seems like the only way out.", 10,false),
+            new Target("Storage Room","door", "","interact", "Your sister yells out in protest. Were you gonna leave without her?", 10,false),
 
             new Target("Storage Room","sister", "","talk", "Let's get out of here!", 10,false),
             new Target("Storage Room","", "","look", "This is definitely a storage room. Lots of shelves filled with gruesome things you don’t really want to look at closely. The door seems like the only way out.", 10,false),
@@ -48,7 +48,7 @@ public class data {
 
             new Target("Hallway (Basement)","door ahead","", "look", "The vines are so dense that you can’t see past them. ", 10,false),
             new Target("Hallway (Basement)","","", "look", "A sizable wooden hallway with a few decorative paintings. It’s dimly lit with a candle on each wall and connects to 4 rooms in total. You notice a cat curled up in front of one of the doors. At the end of the hallway there’s an arched entryway overgrown with vines that spread into the ceiling and the floor.", 10,false),
-            new Target("Hallway (Basement)","cat","", "attack", "You breathe heavily after giving the cat a hard blow. Growling, it jumps to the side and flees to some corner of the room. You don't see where she's gone, because a sparkling something draws all your attention.", 8,true),
+            new Target("Hallway (Basement)","cat","", "attack", "You breathe heavily after giving the cat a hard blow. Growling, it jumps to the side and flees to some corner of the room. You don't see where she's gone, because a sparkling something draws all your attention. You should take a closer look. (button)", 8,true),
             new Target("Hallway (Basement)","sister","", "talk", "If we wouldn’t have gotten knocked out and dumped into a cage I would almost say it’s cozy. But right now this place is scary, I just really want to get out of here. ", 1,false),
             new Target("Hallway (Basement)","sister","", "talk", "There must be a way out of here. Sometimes you need to look at the bigger picture first to understand. ", 1,false),
             new Target("Hallway (Basement)","cat","", "talk", "The cat opens its' eyes groggily and looks at you confused.", 0,false),
@@ -59,11 +59,12 @@ public class data {
             new Target("Hallway (Basement)","cat","", "talk", "Ah, is that so? I don’t remember anyone new getting adopted. ", 2,false),
             new Target("Hallway (Basement)","cat","", "talk", "There’s only one person with authority to enter this room and you are not them. ", 2,false),
             new Target("Hallway (Basement)","cat","", "talk", "I’m taking a nap on my job, so why don’t you let me be?", 2,false),
-            new Target("Hallway (Basement)","cat","", "talk", "You seem suspicious. I will have to get rid off you", 8,true),
+            new Target("Hallway (Basement)","cat","", "talk", "~You seem suspicious. I will have to get rid off you~ said the cat and already you see yourself involved in a fight with the cat. You breathe heavily after giving the cat a hard blow. Growling, it jumps to the side and flees to some corner of the room. You don't see where she's gone, because a sparkling something draws all your attention. You should take a closer look. (button)", 8,true),
 
             //Chapter 2
             //TODO dirty button wird dem Inventar automatisch hinzugefügt
             //TODO Text hinzufügen: You breathe heavily after giving the cat a hard blow. Growling, it jumps to the side and flees to some corner of the room. You don't see where she's gone, because a sparkling something draws all your attention. You pick it up, pluck off the last cat hairs and are the new owner of a dirty button (button).
+            new Target("Hallway (Basement) ", "button", "", "take", "You pick the button up, pluck off the last cat hairs and are the new owner of a dirty button.", 10, true),
             new Target("Hallway (Basement) II","","", "look", "The cat seems to have disappeared. You hold your breath for a moment to listen, but there is nothing but your own excited heartbeat. The hallway has not changed. There are still 4 doors, but the one to the left is now clear of cats (left door). Opposite of it you see a simple wooden door, nothing special, but sturdy (right door). The big arch is still overgrown with plants (door ahead). It doesn't look like they're going to die anytime soon. In your back is the storage room (door behind).", 10,false),
             new Target("Hallway (Basement) II","door ahead","", "look", "The arched entryway is overgrown. The vines are so dense that you can’t see past them.", 10,false),
             new Target("Hallway (Basement) II","door ahead","", "interact", "Even if you could get to the door the huge creepers would make it impossible for you to go through.", 10,false),
